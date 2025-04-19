16f47f3a19415ab766ecb2322135efd3ee50d99d
SGGregory76 <sggregory76@gmail.com>
2025-04-19 05:18:13 -0700

    Delete js/daily-reset-missions.js

--- js/daily-reset-missions.js
+++
@@ -1,5 +0,0 @@
-const today = new Date().toLocaleDateString();
-if (localStorage.getItem("last_reset") !== today) {
-  localStorage.clear(); // or just reset mission keys
-  localStorage.setItem("last_reset", today);
-}
